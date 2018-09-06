f5b2e816abb6cf3180958afdf2ed211566cfb5f0
Adithya Bellathur <bellathu@usc.edu>
2018-09-06 13:36:10 -0700

    Successfully added JUnit testing framework to project. Added test target to Ant

---
+++ tst/csci310/bellathu/project1/MainTest.java
@@ -0,0 +1,14 @@
+package csci310.bellathu.project1;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void initialTest() {
+        assertTrue(true);
+    }
+}
